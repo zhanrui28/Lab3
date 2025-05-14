c61f016731d1e27887a3423e7a25f0fe423801d2
zhanrui <zzhanrui28.24@ichat.sp.edu.sg>
2025-05-14 19:53:33 +0800

    completed adding new pytest functions to test_lab3.py

--- test_Lab3.py
+++ test_Lab3.py
@@ -5,8 +5,8 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [63, 34, 25, 12, 22, 11, 90]
+    test_arr = [11, 12, 22, 25, 34, 63, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
